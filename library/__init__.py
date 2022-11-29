919a4fc1afc732190ec9049d0961e9bb94d9bdf6
dc250601 <92249586+dc250601@users.noreply.github.com>
2022-11-30 02:32:50 +0530

    Endsem_prep

--- library/__init__.py
+++ library/__init__.py
@@ -9,3 +9,4 @@ from . import interpolation
 from .integration import integrate, monte_carlo_integration
 from . import differential_equation
 from .eigen import power_iteration_method as Eigen_Value_Power_Iteration
+from . import decay
--- library/random.py
+++ library/random.py
@@ -1,8 +1,8 @@
 import os
 import sys
 import math
-
-def LGC(a=1103515245, m=2**32, c=12345, no_sample=1, x0=0.1):
+import time
+def LGC(a=1103515245, m=2**32, c=12345, no_sample=1, x0=0.1, repeat = True):
     """
     A function to generate Pseudo Random Number based LCG Algorithm
 
@@ -12,10 +12,22 @@ def LGC(a=1103515245, m=2**32, c=12345, no_sample=1, x0=0.1):
     c: A parameter(increment) used in the algorithm
     no_sample: The number of random numbers to generate, returns a list
     x0: The initial value from which the algorithm should start
+    repeat: If true then return a single random number between 0 and 1
     """
     rand = []
     x = x0
+#     rand.append(x0)
+    if repeat:
+        x = x0*m
+        for i in range(no_sample):
+            x = (a*x+c)%m
+            rand.append(x/m)
+        return rand
+
     for i in range(no_sample):
-        rand.append(x)
         x = (a*x+c)%m
+        rand.append(x/m)
     return rand
+
+    
+
