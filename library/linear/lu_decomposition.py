dbf159615e124144a2172b2378ca20cca0bc57de
dc250601 <92249586+dc250601@users.noreply.github.com>
2022-11-30 13:02:14 +0530

    ensem

--- library/linear/lu_decomposition.py
+++ library/linear/lu_decomposition.py
@@ -1,6 +1,6 @@
 from .. import matrix
 from .. import linalg
-
+import copy
 
 def dolittle(X):
     for j in range(1,X.num_cols+1):
@@ -34,3 +34,20 @@ def lu_solver(X, b):
             sum_ = sum_ + X.matrix[i-1][j-1]*x[j-1]
         x[i-1] = ((1/X.matrix[i-1][i-1])*(y[i-1] - sum_))
     return x,y
+
+def inverter(X):
+    """
+    inverter:Function to invert a given matrix X
+    
+    args:
+    X: A object of type matrix whose inverse is to be found
+    """
+    columns = X.num_cols
+    rows = X.num_rows
+    inv_X = matrix.matrix_init(shape =(rows,columns), scheme="zero")
+    identity = matrix.matrix_init(shape=(rows,columns), scheme="identity")
+    for i in range(columns):
+        identity_column = identity.get_column(i)
+        inverted_column,_ = lu_solver(copy.deepcopy(X),copy.deepcopy(identity_column))
+        inv_X.assign_column(i,inverted_column)
+    return inv_X
